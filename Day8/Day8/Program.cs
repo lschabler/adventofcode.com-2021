3c18d8a3262f1cea06378c74d3d939ad4e4f1b3d
Lukas Schabler <lukas1692@gmail.com>
2021-12-08 21:14:05 +0100

    Day 8 https://adventofcode.com/2021/day/8

---
+++ Day8/Day8/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 8!");
+
+            string[] lines = System.IO.File.ReadAllLines(@"input.txt");
+            
+            var elements = new List<SevenSegmentInput>();
+            foreach (var item in lines)
+                elements.Add(new SevenSegmentInput(item));
+
+            // part 1
+            int num_sum = 0;
+            foreach (var item in elements)
+            {
+                num_sum += item.GetBasicValues().Length;
+                //Console.WriteLine(string.Join(",", item.GetBasicValues()));
+            }
+            Console.WriteLine("Part1: " + num_sum);
+
+            
+            int num_sum_2 = 0;
+            // part 2
+            foreach (var item in elements)
+            {
+                var decoded = item.GetDecodedValues();
+                //num_sum += item.GetDecodedValues().Length;
+                int res = 0;
+                int dec = 1;
+                for (int i = decoded.Length-1; i >= 0; i--)
+                {
+                    res += dec * decoded[i];
+                    dec *= 10;
+                }
+                Console.WriteLine(res);
+                num_sum_2 += res;
+
+                //Console.WriteLine(string.Join(item.GetDecodedValues()));
+            }
+            Console.WriteLine("Part2: " + num_sum_2);
+
+        }
+    }
+}
---
+++ Day8/Day8/SevenSegmentInput.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day8
+{
+    class SevenSegmentInput
+    {
+        string[] seven_segment_;
+        string[] output_value_;
+        int[] output_solved_;
+
+        public SevenSegmentInput(string input)
+        {
+            var parts = input.Split('|');
+            seven_segment_ = parts[0].Split(' ').Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            output_value_ = parts[1].Split(' ').Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            output_solved_ = new int[output_value_.Length];
+        }
+
+        // Part 1, digits 1, 4, 7, and 8
+        public int[] GetBasicValues()
+        {
+            var ret = new List<int>();
+
+            int index = 0;
+            foreach (var item in output_value_)
+            {
+                output_solved_[index] = -1;
+
+                if (item.Length == 2)
+                {
+                    ret.Add(1);
+                    output_solved_[index] = 1;
+                }  
+                else if (item.Length == 4)
+                {
+                    ret.Add(4);
+                    output_solved_[index] = 4;
+                } 
+                else if (item.Length == 3)
+                {
+                    ret.Add(7);
+                    output_solved_[index] = 7;
+                } 
+                else if (item.Length == 7)
+                {
+                    ret.Add(8);
+                    output_solved_[index] = 8;
+                }
+                index++;
+            }
+
+            return ret.ToArray();
+        }
+
+        public int[] GetDecodedValues()
+        {
+            int[] seven_solved = new int[seven_segment_.Length];
+            SortedDictionary<int, string> canditates = new SortedDictionary<int, string>();
+            // parse 1, 4, 7, and 8
+            for (int i = 0; i < seven_segment_.Length; i++)
+            {
+                seven_solved[i] = -1;
+                if (seven_segment_[i].Length == 2)
+                {
+                    seven_solved[i] = 1;
+                    canditates[1] = seven_segment_[i];
+                }              
+                if (seven_segment_[i].Length == 4)
+                {
+                    seven_solved[i] = 4;
+                    canditates[4] = seven_segment_[i];
+                }
+                if (seven_segment_[i].Length == 3)
+                {
+                    seven_solved[i] = 7;
+                    canditates[7] = seven_segment_[i];
+                }
+                if (seven_segment_[i].Length == 7)
+                {
+                    seven_solved[i] = 8;
+                    canditates[8] = seven_segment_[i];
+
+                }
+
+            }
+            
+            // find 3 or 6
+            for (int i = 0; i < seven_segment_.Length; i++)
+            {
+                if (seven_solved[i] >= 0) continue;
+                var cand = canditates[1];
+                var seven = seven_segment_[i];
+                int index1 = seven.IndexOf(cand[0]);
+                int index2 = seven.IndexOf(cand[1]);
+                // find 2
+                if (seven.Length == 5 && index1 > -1 && index2 > -1)
+                {
+                    seven_solved[i] = 3;
+                    canditates[3] = seven;
+                }
+                if (seven.Length == 6 && (index1 == -1 || index2 == -1))
+                {
+                    seven_solved[i] = 6;
+                    canditates[6] = seven;
+                }
+
+                //int index1 = str.IndexOf('F');
+            }
+
+            
+            // 0 9 -> 6
+            for (int i = 0; i < seven_segment_.Length; i++)
+            {
+                if (seven_solved[i] >= 0) continue;
+                var seven = seven_segment_[i];
+                if (seven.Length != 6) continue;
+                var cand = canditates[3];
+                int count = 0;
+                foreach (var item in cand)
+                {
+                    if (seven.IndexOf(item)>=0)
+                        count++;
+                }
+                if(count == cand.Length)
+                {
+                    seven_solved[i] = 9;
+                    canditates[9] = seven;
+                }
+                else
+                {
+                    seven_solved[i] = 0;
+                    canditates[0] = seven;
+                }
+            }
+
+            // 2 5 -> 5
+            for (int i = 0; i < seven_segment_.Length; i++)
+            {
+                if (seven_solved[i] >= 0) continue;
+                var seven = seven_segment_[i];
+                if (seven.Length != 5) continue;
+                var cand = canditates[9];
+                int count = 0;
+                foreach (var item in cand)
+                {
+                    if (seven.IndexOf(item) >= 0)
+                        count++;
+                }
+                if (count == cand.Length-1) // 5
+                {
+                    seven_solved[i] = 5;
+                    canditates[5] = seven;
+                }
+                else
+                {
+                    seven_solved[i] = 2;
+                    canditates[2] = seven;
+                }
+
+            }
+
+            string[] output_sorted = new string[output_value_.Length];
+            for (int i = 0; i < output_value_.Length; i++)
+            {
+                string out_val = output_value_[i];
+                var sorted = string.Concat(out_val.OrderBy(x => x));
+                output_sorted[i] = sorted;
+            }
+
+            for (int i = 0; i < canditates.Keys.Count; i++)
+            {
+                var item = canditates.Keys.ElementAt(i);
+                string code = canditates[item];
+                var sorted = string.Concat(code.OrderBy(x=>x));
+                canditates[item] = sorted;
+
+                for (int j = 0; j < output_sorted.Length; j++)
+                {
+                    if (output_sorted[j] == sorted)
+                    {
+                        output_solved_[j] = i;
+                        //Console.WriteLine("Found: " + i + " " + sorted);
+                    }
+                        
+                }
+            }
+
+            return output_solved_;
+        }
+    }
+}
